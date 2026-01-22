f610508b51cb0cbcac164134722a1dd76d9d5641
Kyookusanagi <afifmp01@gmail.com>
2026-01-22 14:03:33 +0800

    Commit kedua

---
+++ buku/create.php
@@ -0,0 +1,35 @@
+<?php
+
+include __DIR__ . '/../koneksi.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $judul = $_POST['judul'];
+    $pengarang = $_POST['pengarang'];
+    $stok = $_POST['stok'];
+    $sql = "INSERT INTO buku (judul, pengarang, stok) VALUES (?,?,?)";
+    $result = $koneksi-> execute_query($sql, [$judul, $pengarang, $stok]);
+
+    if ($result) {
+        header('Location: index.php');
+    }
+}
+?>
+
+<h1>Tambah Buku Baru</h1>
+<form action="" method="post">
+    <div class="form-item">
+        <label for="judul">Judul Buku</label>
+        <input type="text" name="judul" id="judul" required>
+    </div>
+
+    <div class="form-item">
+        <label for="pengarang">Pengarang</label>
+        <input type="text" name="pengarang" id="pengarang" required>
+    </div>
+
+    <div class="form-item">
+        <label for="stok">Stok</label>
+        <input type="number" name="stok" id="stok" required>
+    </div>
+
+    <button type="submit">Tambah</button>
+</form>
---
+++ buku/index.php
@@ -0,0 +1,38 @@
+<?php
+include __DIR__ . '/../koneksi.php';
+$sql = "SELECT * FROM buku";
+$books = $koneksi->query($sql);
+$books = $books->fetch_all(MYSQLI_ASSOC);
+$no = 1;
+?>
+
+<h1>Data buku</h1>
+<a href="create.php">Tambah Buku</a>
+
+<table border="1">
+<thead>
+    <tr>
+        <th>No</th>
+        <th>Judul</th>
+        <th>Pengarang</th>
+        <th>Stok</th>
+        <th>Aksi</th>
+    </tr>
+</thead>
+<tbody>
+
+<?php foreach ($books as $book): ?>
+<tr>
+    <td><?= $no++; ?></td>
+    <td><?= $book['judul']; ?></td>
+    <td><?= $book['pengarang']; ?></td>
+    <td><?= $book['stok']; ?></td>
+    <td>
+        <a href="edit.php">Edit</a> |
+        <a href="delete.php">Hapus</a>
+    </td>
+</tr>
+<?php endforeach; ?>
+
+</tbody>
+</table>
